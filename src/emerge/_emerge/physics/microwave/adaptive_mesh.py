Fixed a bug with copying geometries (priority), slight improvements on adaptive mesh refinement and S-parameter calculation

src/emerge/_emerge/physics/microwave/adaptive_mesh.py
@@ -224,22 +224,11 @@ def select_refinement_indices(errors: np.ndarray, refine: float) -> np.ndarray:
         np.ndarray of indices (ints) sorted by decreasing error.
     """
     # Flatten and sanitize
-    errs = np.abs(np.ravel(errors).astype(float))
-    n = errs.size
-    if n == 0:
-        return np.empty(0, dtype=int)
-
-    errs[~np.isfinite(errs)] = 0.0  # replace NaN/inf by 0
-    theta = float(np.clip(refine, 0.0, 1.0))
-    if theta <= 0.0:
-        return np.empty(0, dtype=int)
-
-    # Dörfler uses squared indicators
+    errs = np.abs(errors)
+    
     ind = errs * errs
-    total = ind.sum()
-    if total <= 0.0:
-        return np.empty(0, dtype=int)
-
+    total = np.sum(ind)
+    
     # Sort by decreasing indicator
     order = np.argsort(ind)[::-1]
     sum_error = 0
@@ -247,16 +236,10 @@ def select_refinement_indices(errors: np.ndarray, refine: float) -> np.ndarray:
     for index in order:
         sum_error += ind[index]
         indices.append(index)
-        if sum_error >= refine*total and len(indices) >= 10:
+        if sum_error >= refine*total and len(indices) >= 20:
             break
     
-    #cum = np.cumsum(ind[order])
-
-    # Smallest m with cumulative ≥ theta * total
-    #m = int(np.searchsorted(cum, theta * total, side="left")) + 1
-
-    #chosen = order[:m]                # already from largest to smallest
-    return np.array(indices)#chosen.astype(int)
+    return np.array(indices)
 
 @njit(f8[:](i8, f8[:,:], f8, f8, f8[:]), cache=True, nogil=True, parallel=False)
 def compute_size(index: int, coords: np.ndarray, gr: float, scaler: float, dss: np.ndarray) -> float:

src/emerge/_emerge/physics/microwave/microwave_3d.py
@@ -1180,14 +1180,13 @@ class Microwave3D:
                 # Compute the S-parameters
                 # Define the field interpolation function
                 fieldf = self.basis.interpolate_Ef(solution, tetids=port_tets)
-                Pout = 0.0 + 0j
 
                 # Active port power
                 tris = mesh.get_triangles(active_port.tags)
                 tri_vertices = mesh.tris[:,tris]
-                pfield, pmode = self._compute_s_data(active_port, fieldf, tri_vertices, k0, ertri[:,:,tris], urtri[:,:,tris])
-                logger.debug(f'[{active_port.port_number}] Active port amplitude = {np.abs(pfield):.3f} (Excitation = {np.abs(pmode):.2f})')
-                Pout = pmode
+                EdotF_act, EdotE_act = self._compute_s_data(active_port, fieldf, tri_vertices, k0, ertri[:,:,tris], urtri[:,:,tris])
+                logger.debug(f'[{active_port.port_number}] Active port amplitude = {np.abs(EdotF_act):.3f} (Excitation = {np.abs(EdotE_act):.2f})')
+                Amp_act = np.sqrt(active_port.power)
                 
                 #Passive ports
                 for bc in all_ports:
@@ -1195,13 +1194,14 @@ class Microwave3D:
                     fieldf = self.basis.interpolate_Ef(solution, tetids=port_tets)
                     tris = mesh.get_triangles(bc.tags)
                     tri_vertices = mesh.tris[:,tris]
-                    pfield, pmode = self._compute_s_data(bc, fieldf,tri_vertices, k0, ertri[:,:,tris], urtri[:,:,tris])
-                    logger.debug(f'[{bc.port_number}] Passive amplitude = {np.abs(pfield):.3f}')
-                    scalardata.write_S(bc.port_number, active_port.port_number, pfield/Pout)
-                    if abs(pfield/Pout) > 1.0:
-                        logger.warning(f'S-parameter ({bc.port_number},{active_port.port_number}) > 1.0 detected: {np.abs(pfield/Pout)}')
+                    EdotF_pas, EdotE_pas = self._compute_s_data(bc, fieldf,tri_vertices, k0, ertri[:,:,tris], urtri[:,:,tris])
+                    Amp_pas = EdotF_pas/EdotE_pas
+                    logger.debug(f'[{bc.port_number}] Passive amplitude = {np.abs(EdotF_pas):.3f}')
+                    scalardata.write_S(bc.port_number, active_port.port_number, Amp_pas/Amp_act)
+                    if abs(Amp_pas/Amp_act) > 1.0:
+                        logger.warning(f'S-parameter ({bc.port_number},{active_port.port_number}) > 1.0 detected: {np.abs(Amp_pas/Amp_act)}')
                         not_conserved = True
-                        conserve_margin = abs(pfield/Pout) - 1.0
+                        conserve_margin = abs(Amp_pas/Amp_act) - 1.0
                 active_port.active=False
             
             
@@ -1272,7 +1272,7 @@ class Microwave3D:
                 a = bc.voltage
                 b = (V-bc.voltage)
             else:
-                a = 0
+                a = bc.voltage
                 b = V
             
             a_sig = a*csqrt(1/(2*bc.Z0))
@@ -1287,8 +1287,8 @@ class Microwave3D:
             elif bc.modetype(k0) == 'TM':
                 const = 1/((erp[0,0,:] + erp[1,1,:] + erp[2,2,:])/3)
             const = np.squeeze(const)
-            field_p = sparam_field_power(self.mesh.nodes, tri_vertices, bc, k0, fieldfunction, const, 5)
-            mode_p = sparam_mode_power(self.mesh.nodes, tri_vertices, bc, k0, const, 5)
+            field_p = sparam_field_power(self.mesh.nodes, tri_vertices, bc, k0, fieldfunction, const, 4)
+            mode_p = sparam_mode_power(self.mesh.nodes, tri_vertices, bc, k0, const, 4)
             return field_p, mode_p
 
 