d587dc91793a7ebfbf00b60df42e70aec5648c87
Robert Fennis <robertfennis@Mac.home>
2025-12-19 13:03:52 +0100

    Bump version: 1.3.4 → 1.4.0

--- src/emerge/__init__.py
+++ src/emerge/__init__.py
@@ -34,7 +34,7 @@ warnings.filterwarnings(
 
 import os
 
-__version__ = "1.3.4"
+__version__ = "1.4.0"
 
 NTHREADS = "1"
 os.environ["EMERGE_STD_LOGLEVEL"] = os.getenv("EMERGE_STD_LOGLEVEL", default="INFO")
